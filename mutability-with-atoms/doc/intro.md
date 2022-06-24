ea15c8df1d0abe2fc6b6cdc4eb4d4ee29c7c1fc8
Marco Calzada <marco.calzada@nubank.com.br>
2022-06-24 14:33:13 -0500

    chore(new module): adding mutability with atoms module

---
+++ mutability-with-atoms/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mutability-with-atoms
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ mutability-with-atoms/test/mutability_with_atoms/core_test.clj
@@ -0,0 +1,7 @@
+(ns mutability-with-atoms.core-test
+  (:require [clojure.test :refer :all]
+            [mutability-with-atoms.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
