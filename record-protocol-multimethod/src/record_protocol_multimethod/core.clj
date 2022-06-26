8b3a8d533c22889e42ea5423ad80ed9520f54982
Marco Calzada <marco.calzada@nubank.com.br>
2022-06-25 23:51:53 -0500

    chore(new course): add record-protocol-multimethod course structure

---
+++ record-protocol-multimethod/src/record_protocol_multimethod/core.clj
@@ -0,0 +1,6 @@
+(ns record-protocol-multimethod.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ record-protocol-multimethod/test/record_protocol_multimethod/core_test.clj
@@ -0,0 +1,7 @@
+(ns record-protocol-multimethod.core-test
+  (:require [clojure.test :refer :all]
+            [record-protocol-multimethod.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
